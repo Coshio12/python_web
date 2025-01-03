03d548cab295e031f813ba8bef1bab39af512762
Coshio12 <alejandrocossio3327@gmail.com>
2025-01-03 11:38:13 -0400

    Listas inicio

---
+++ ejercicios_basicos/colecciones.py
@@ -0,0 +1,7 @@
+#Las colecciones son aquellas que tienen la posibilidad de guardar diferente informacion en una sola variable haciendo posible el acceso a la misma
+coleccion1 = {
+    'nombre': 'Jose',
+    'apellido' : 'Cossio'
+}
+
+print(coleccion1)
