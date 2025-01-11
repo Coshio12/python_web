f436c24db62ac0c7569ccc65f7b3d935fbaa5f0d
Coshio12 <alejandrocossio3327@gmail.com>
2025-01-10 21:34:12 -0400

    Mas sobre funciones

--- 03_funciones/1-funciones.py
+++ 03_funciones/1-funciones.py
@@ -51,30 +51,32 @@ saludar_4()
 print('La variable global esta aqui: ',nombre_apellido)
 #Con la variable global permitara usar una variable en cualquier parte de la aplicacion
 
-#Retornando valores
-#La frase de retornar valores hace incapie a que a partir de una funcion podamos retornar los valores
-#Es decir, que dentro de una funcion se ejecuten todas las operaciones deseadas
-#Y la misma funcion las devuelva
-#Para ello se usara la palabra reservada de return, que permite retornar un valor final
 
-#Definimos la nueva funcion
-def retorno():
-    #Definimos las variables
-    global nombre_2
-    nombre_2 = 'Jose Cossio'
-    edad = 23
-    #Retornamos un saludo, el nombre y la edad
-    return 'Hola desde la funcion saludar ', nombre_2, edad
+#PARAMETROS Y ARGUMENTOS
+def saludo(nombre):#parametros
+    edad = 25
+    return f'Hola, {nombre} desde la funcion SALUDO'
 
-#Podemos retornar el valor de la funcion solamente llamando dicha funcion dentro de un print
-print(retorno())
+def sumar(numero1, numero2):
+    return numero1 + numero2
 
-#Podemos crear una variable y guardar la informacion de la funcion dentro de ella
-#En este caso seria una tupla ya que esta recibiendo 3 valores distintos
-valor = retorno() 
-print(valor)
+def restar(numero1, numero2):
+    return numero1- numero2
 
-#Al retornar una tupla, podemos crear varias variables para que guarden cada valor de la tupla
-saludo, nombre, edad = retorno()
-#Y podemos imprimirla en un print formateando la infomacion
-print(f'El saludo "{saludo}" el nombre es: {nombre} y la edad {edad}')
+def restando(a = None, b = None): #parametros por defecto
+    if a == None or b == None:
+        print('Debes enviar dos numeros a la funcion')
+        return
+    return a - b
+
+saludando = saludo('Alejandro') #argumentos
+print(saludando)
+
+suma = sumar(12,8) #argumentos por posicion
+print(suma)
+
+resta = restar(numero2=20, numero1=40) #argumentos por nombre
+print(resta)
+
+resta = restando() 
+print(resta)
