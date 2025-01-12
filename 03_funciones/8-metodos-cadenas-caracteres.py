0f5c49cfc29323fe064fc121c32a547ad3a61298
Coshio12 <alejandrocossio3327@gmail.com>
2025-01-11 22:14:55 -0400

    metodos con caracteres

---
+++ 03_funciones/8-metodos-cadenas-caracteres.py
@@ -0,0 +1,40 @@
+"""
+
+"""
+print('Hola mundo'.upper())
+
+print('Hola mundo'.lower())
+
+print('hola mundo'.capitalize())
+
+print('hola mundo'.title())
+
+print('Hola mundo'.count('o'))
+
+palabra = 'Python'
+palabra = palabra.replace('P','S')
+print(palabra)
+
+a = 'P Y T H O N'.replace(' ','')
+print(a)
+
+h = '   Hola Mundo  '.strip()
+print(h)
+
+h = '---------Hola - Mundo----------'.strip('-')
+print(h)
+
+h = 'Hola Mundo'.split()
+print(h)
+
+h = 'Hola,Mundo,de,python'.split(',')
+print(h)
+
+print('Hola'.islower())
+print('hola'.islower())
+print('Hola'.isupper())
+print('HOLA'.islower())
+print('Hola Mundo'.istitle())
+print('hola mundo'.istitle())
+print('           '.isspace())
+print('      s     '.isspace())
