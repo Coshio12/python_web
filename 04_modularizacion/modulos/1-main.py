bda03d0c62b49fa00875457f028b3dd88ebaaf3c
Coshio12 <alejandrocossio3327@gmail.com>
2025-01-17 21:46:28 -0400

    Formateando info y funcion dir

--- 04_modularizacion/modulos/1-main.py
+++ 04_modularizacion/modulos/1-main.py
@@ -36,3 +36,31 @@ from fibonacci import fibo as f1
 from fibonacci import fibo2 as f2
 f1(650)
 print(f2(650))
+
+#Siguiendo el tema de los modulos, exixte una funcion que nos da la informacion que contiene un modulo
+#Como ser funciones, etc
+#Esta es la funcion dir que puede recibir como parametros las librerias que deseamos importar 
+#Por ejemplo vamos a importar el modulo de fibonacci
+import fibonacci
+
+#Y vamos a pasarle a la funcion fibonacci
+print(dir(fibonacci))
+#Lo que nos devuelve es esto:
+#['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'fibo', 'fibo2']
+#Dandonos a conocer todo lo que contiene fibonacci
+
+#Tambien podemos hacerlo con sys que lo vimos en otro archivo (entra-script.py)
+import sys
+print(dir(sys))
+print('='*45)
+#Ejecutalo para ver el resultado que es una lista de todo lo que contiene
+#Hacemos lo mismo con otro modulo
+import builtins
+print(dir(builtins))
+print('='*45)
+
+#O tambien podemos solo imprimir dir() sin ningun argumento
+print(dir())
+
+#NOTA: ESTE ARCHIVO ESTA SIGUIENDO MALAS PRACTICAS DE CODIGO LIMPIO
+#SE LO CREO DE ESA FORMA PARA SEGUIR UNA COHERENCIA Y FACIL ENTENDIMIENTO
