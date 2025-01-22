ff0e665600af86d286829adf830814f099c9f08d
Coshio12 <alejandrocossio3327@gmail.com>
2025-01-22 19:16:25 -0400

    Conexion a base de datos

--- 07_proyecto_tkinder/catalogo-peliculas/catalogo-peliculas/client/gui_app.py
+++ 07_proyecto_tkinder/catalogo-peliculas/catalogo-peliculas/client/gui_app.py
@@ -2,6 +2,10 @@
 #Importamos tkinder
 import tkinter as tk
 
+from tkinter import ttk
+
+from model.pelicula_dao import crear_tabla,borrar_tabla
+
 #Creamos una barra de menu que reciba la raiz
 def barra_menu(root):
     #Creamos un objeto de menu que recibe la riaz
@@ -16,8 +20,8 @@ def barra_menu(root):
     barra_menu.add_cascade(label='Inicio', menu = menu_inicio)
     
     #Añadimos componentes al menu despegable de inicio con add_command
-    menu_inicio.add_command(label='Crear Registro')
-    menu_inicio.add_command(label='Eliminar Registro')
+    menu_inicio.add_command(label='Crear Registro', command=crear_tabla)
+    menu_inicio.add_command(label='Eliminar Registro', command=borrar_tabla)
     #A este ultimo que es salir le damos la funcionalidad de destroy
     #Esto hara que se cierre la ventana
     menu_inicio.add_command(label='Salir', command = root.destroy)
@@ -44,6 +48,11 @@ class Frame(tk.Frame):
         #Agregamos al constructor todo lo que tendra campos de peliculas
         self.campos_pelicula()
         
+        #Agregamos el metodo de desabilitar los campos
+        self.desabilitar_campos()
+        
+        self.tabla_peliculas()
+        
     #Labels de campos de pelicula
     def campos_pelicula(self):
         #Labels de cada campo
@@ -69,24 +78,34 @@ class Frame(tk.Frame):
         
         #Entrys, campos de entrada de la informacion
         #Creamos un objeto del tipo entry con el self ya que esta en una clase frame
-        self.entry_nombre = tk.Entry(self)
+        #Tambien hay que darle funcionalidad a los botones para ello cada que ejecutemos el boton cancelar o nuevo
+        #tenemos que pasarle datos vacios para ello eusaremos el objeto StringVar en una nueva variable
+        self.mi_nombre = tk.StringVar()
+        #Y esta variable le pasamos como argumento cada que creemos el entry con textvariable
+        #Y repetimos con los otros entrys
+        self.entry_nombre = tk.Entry(self, textvariable= self.mi_nombre)
         #Creamos la personalizacion delentry con el state de deshabilitado y personalizamos el estilo del texto
-        self.entry_nombre.config(width=50, state='disabled', font= ('Arial',12))
+        self.entry_nombre.config(width=50, font= ('Arial',12))
         #Lo agregamos a la grid pero modificando la columna
         self.entry_nombre.grid(row=0, column=1,columnspan=2, padx= 10, pady= 10)
         #Este mismo proceso lo repetimos para los otros campos
         
-        self.entry_duracion = tk.Entry(self)
-        self.entry_duracion.config(width=50, state='disabled', font=('Arial',12))
+        self.mi_duracion = tk.StringVar()
+        self.entry_duracion = tk.Entry(self, textvariable=self.mi_duracion)
+        self.entry_duracion.config(width=50, font=('Arial',12))
         self.entry_duracion.grid(row=1, column=1,columnspan=2, padx=10, pady=10)
         
-        self.entry_genero = tk.Entry(self)
-        self.entry_genero.config(width=50, state='disabled', font=('Arial',12))
+        self.mi_genero = tk.StringVar()
+        self.entry_genero = tk.Entry(self,textvariable=self.mi_genero)
+        self.entry_genero.config(width=50, font=('Arial',12))
         self.entry_genero.grid(row=2, column=1,columnspan=2, padx=10, pady=10)
         
         #BOTONES DE GUARDAR, CANCELAR,NUEVO
         #NUEVO
-        self.boton_nuevo = tk.Button(self, text= 'NUEVO')
+        #Al ser el boton nuevo queremos que el usuario digite valores
+        #entonces tenemos que habilitar los campos y para ello debemos agregar el parametro
+        #command con el metodo para que se habiliten los campos
+        self.boton_nuevo = tk.Button(self, text= 'NUEVO',command= self.habilidar_campos)
         self.boton_nuevo.config(
             width=20, 
             bg='green',
@@ -98,7 +117,9 @@ class Frame(tk.Frame):
         self.boton_nuevo.grid(row=3,column=0, padx=10, pady=10)#Dibuja el boton
         
         #GUARDAR
-        self.boton_guardar = tk.Button(self, text= 'GUARDAR')
+        #Al ser el boton guardar queremos que los entry esten vacios despues  de guardar la informacion
+        #entonces tenemos que pasarle datos vacios y despues de guardar que se desabiliten los campos
+        self.boton_guardar = tk.Button(self, text= 'GUARDAR',command=self.guardar_datos)
         self.boton_guardar.config(
             width=20, 
             bg='#1658A2',
@@ -110,7 +131,9 @@ class Frame(tk.Frame):
         self.boton_guardar.grid(row=3,column=1, padx=10, pady=10)#Dibuja el boton
         
         #CANCELAR
-        self.boton_cancelar = tk.Button(self, text= 'CANCELAR')
+        #Al ser el boton cancelar queremos desabilitar los campos
+        #Para ello debemos agregar el parametro command con el metodo para que se habiliten los campos
+        self.boton_cancelar = tk.Button(self, text= 'CANCELAR',command=self.desabilitar_campos)
         self.boton_cancelar.config(
             width=20, 
             bg='#BD152E',
@@ -120,5 +143,76 @@ class Frame(tk.Frame):
             activebackground='#E15370'
             )
         self.boton_cancelar.grid(row=3,column=2, padx=10, pady=10)#Dibuja el boton
+    
+    #Creamos el metodo de habilitar campos
+    def habilidar_campos(self):
+        #Le pasamos a los campos datos vacios
+        self.mi_nombre.set('')
+        self.mi_duracion.set('')
+        self.mi_genero.set('')
+        
+        #Y a los diferentes entrys le damos una funcionalidad que en este caso es cambiar el estado
+        #como estamos habilitando, el state tiene que ser normal
+        self.entry_nombre.config(state='normal')
+        self.entry_duracion.config(state='normal')
+        self.entry_genero.config(state='normal')
+        
+        self.boton_guardar.config(state='normal')
+        self.boton_cancelar.config(state='normal')
+    
+    #Creamos el metodo de desahabilitar campos
+    def desabilitar_campos(self):
+        #Le pasamos a los campos datos vacios
+        self.mi_nombre.set('')
+        self.mi_duracion.set('')
+        self.mi_genero.set('')
+        
+        #Y a los diferentes entrys le damos una funcionalidad que en este caso es cambiar el estado
+        #como estamos desahabilitando, el state tiene que ser disabled
+        self.entry_nombre.config(state='disabled')
+        self.entry_duracion.config(state='disabled')
+        self.entry_genero.config(state='disabled')
+        
+        self.boton_guardar.config(state='disabled')
+        self.boton_cancelar.config(state='disabled')
+    
+    def guardar_datos(self):
+        self.desabilitar_campos()
+        
+    def tabla_peliculas(self):
+        self.tabla = ttk.Treeview(self, 
+            column= ('NOMBRE', 'DURACION','GENERO'))
+        
+        self.tabla.grid(row=4,column=0, columnspan=4,)
+        
+        self.tabla.heading('#0',text='ID')
+        self.tabla.heading('#1',text='NOMBRE')
+        self.tabla.heading('#2',text='DURACION')
+        self.tabla.heading('#3',text='GENERO')
+        
+        self.tabla.insert('',0,text='1',
+            values=('Los vengadores','2.35','Ciencia ficcion'))
+        
+        self.boton_editar = tk.Button(self, text= 'EDITAR')
+        self.boton_editar.config(
+            width=20, 
+            bg='green',
+            font=('Time New Roman',12,'bold'),
+            fg='white', 
+            cursor='hand2',
+            activebackground='#35BD6F'
+            )
+        self.boton_editar.grid(row=5,column=0, padx=10, pady=10)#Dibuja el boton
+        
+        self.boton_eliminar = tk.Button(self, text= 'ELIMINAR')
+        self.boton_eliminar.config(
+            width=20, 
+            bg='#BD152E',
+            font=('Time New Roman',12,'bold'),
+            fg='white', 
+            cursor='hand2',
+            activebackground='#E15370'
+            )
+        self.boton_eliminar.grid(row=5,column=1, padx=10, pady=10)#Dibuja el boton
         
         
---
+++ 07_proyecto_tkinder/catalogo-peliculas/catalogo-peliculas/model/pelicula_dao.py
@@ -0,0 +1,27 @@
+from .conexion_db import ConexionDB
+
+def crear_tabla():
+    conexion = ConexionDB()
+    
+    sql = '''
+    CREATE TABLE peliculas(
+        id_pelicula INTEGER,
+        nombre_pelicula VARCHAR(150),
+        duracion_pelicula VARCHAR(150),
+        genero_pelicula VARCHAR(150),
+        
+        PRIMARY KEY (id_pelicula AUTOINCREMENT)
+    )
+    '''
+    
+    conexion.cursor.execute(sql)
+    conexion.cerrar()
+    
+def borrar_tabla():
+    conexion = ConexionDB()
+    
+    sql='DROP TABLE peliculas'
+    
+    conexion.cursor.execute(sql)
+    conexion.cerrar()
+    
